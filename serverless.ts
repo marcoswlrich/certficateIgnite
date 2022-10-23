dab3a7cc5a8bb894c9e9aaa0348d831b9341e9f8
marcoswlrich <marcosapw.dev@gmail.com>
2022-10-23 17:30:23 -0400

    Config

--- serverless.ts
+++ serverless.ts
@@ -1,14 +1,14 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
   service: 'certificateignite',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  frameworkVersion: '2',
+  plugins: ['serverless-esbuild', 'serverless-dynamodb-local', 'serverless-offline'],
   provider: {
     name: 'aws',
-    runtime: 'nodejs14.x',
+    runtime: 'nodejs16.x',
+    region: 'eu-west-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -17,10 +17,47 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    lambdaHashingVersion: '20201221',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: ['dynamodb:*'],
+        Resource: ['*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:*'],
+        Resource: ['*'],
+      },
+    ],
   },
-  // import the function via paths
-  functions: { hello },
-  package: { individually: true },
+  package: { individually: false, include: ['./src/templates/**'] },
+  functions: {
+    generateCertificate: {
+      handler: 'src/functions/generateCertificate.handler',
+      events: [
+        {
+          http: {
+            path: 'generateCertificate',
+            method: 'post',
+            cors: true,
+          },
+        },
+      ],
+    },
+    verifyCertificate: {
+      handler: 'src/functions/verifyCertificate.handler',
+      events: [
+        {
+          http: {
+            path: 'verifyCertificate/{id}',
+            method: 'get',
+            cors: true,
+          },
+        },
+      ],
+    },
+  },
   custom: {
     esbuild: {
       bundle: true,
@@ -31,6 +68,41 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+      external: ['chrome-aws-lambda'],
+    },
+    dynamodb: {
+      stages: ['dev', 'local'],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      dbCertificateUsers: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'users_certificate',
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: 'id',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'id',
+              KeyType: 'HASH',
+            },
+          ],
+        },
+      },
     },
   },
 };
